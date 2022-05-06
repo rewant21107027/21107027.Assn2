aae1421f4fc71fc2ef927a0cde9bb749c95eeb6d
rewant21107027 <105057094+rewant21107027@users.noreply.github.com>
2022-05-06 19:31:46 +0530

    Add files via upload

---
+++ Assn2.21107027 output.py
@@ -0,0 +1,42 @@
+"C:\python 3.10\python.exe" C:/Users/Legion/PycharmProjects/assn2.21107027/21107027Assn2.py
+<A> the length of the string_1 is: 35
+<B> the reverse of order of string_1 is: egaugnal evitisnes esac a si nohtyp
+<E> the index of substring a in string_a is: 10
+<F> the input string string_a after removing white spaces is: pythonisacasesensitivelanguage
+
+
+enter your name:Rewant Pahuja
+enter your SID:21107027
+enter your department:Mechanical
+enter your CGPA:10
+hey, Rewant Pahuja  here!
+my SID is 21107027
+i am from Mechanical and my CGPA is 10.000000
+
+
+<a>  8
+<b> 58
+<c> 50
+<d> left shift both a and b with 2 bits respectively are: 224 40
+<e> right shift a with 2 bits and b with 4 bits respectively are: 14 0
+
+
+Enter a string:my name is rewant
+Yes
+
+
+This program determines whether 3 sides entered by the user can form triangle or not?
+Enter length of first side of the triangle:3
+Enter length of second side of the triangle:4
+Enter length of third side of the triangle:5
+Can  the sides entered by user form a triangle?
+The answer is: Yes
+
+
+Enter the value of a:5
+Enter the value of b:8
+1
+2
+3
+
+Process finished with exit code 0
