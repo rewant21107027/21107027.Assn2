2d2443b7ba8764cd8f6cb75bcb9963789854fdab
rewant21107027 <105057094+rewant21107027@users.noreply.github.com>
2022-05-12 20:31:54 +0530

    Create Assn3.21107027 output.py

---
+++ Assn3.21107027 output.py
@@ -0,0 +1,76 @@
+"C:\python 3.10\python.exe" C:/Users/Legion/PycharmProjects/Assn3.21107027/assn3.21107027.py
+Enter your input as decimal number and get its binary equivalent
+enter your decimal number: 27
+The binary equivalent is:
+1 1 0 1 1 
+
+Welcome to this interactive python calculator
+Enter the expressions by using only these operators (+,-,*,/,**,%,//):(5**2)+(5-2)*8*9-(8//2)+(7%3)
+The result is  238
+
+
+Result of part (a) is:  35
+
+
+Choose the value of n
+The value of n taken is: 4
+The result of part (b) is:  6.0
+
+
+Choose the value of r
+The value of r1 taken in: 2
+The result of part (c) is:  50.26548245743669
+enter the value of r: 6
+enter the value of angle a: 45
+enter the value of angle b: 30
+The result of part (d) is:  2.740988524758533
+
+
+enter the value of x1:4
+enter the value of x2:6
+enter the value of y1:3
+enter the value of y2:1
+The result of part (e) is:  (y2-y1)/(x2-x1) -0.5
+
+
+In the range(5): 
+0
+1
+2
+3
+4
+In the range(3, 10): 
+3
+4
+5
+6
+7
+8
+9
+
+
+In the range(4, 13, 3): 
+4
+7
+10
+
+
+In the range(15 ,5, -2): 
+15
+13
+11
+9
+7
+
+
+In the range(5, 3): 
+
+
+This program computes molecular weight of carbohydrates
+, for this we need to enter number of atoms of hydrogen, carbon and oxygen.
+enter the number of hydrogen atoms: 2
+enter the number of carbon atoms:3
+enter the number of oxygen atoms:4
+The weight of carbohydrate:  102.04558
+
+Process finished with exit code 0
