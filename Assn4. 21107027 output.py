0055fbdcc0283a69340bc423a98201fc190196b2
rewant21107027 <105057094+rewant21107027@users.noreply.github.com>
2022-05-27 20:06:33 +0530

    Create Assn4. 21107027 output.py

---
+++ Assn4. 21107027 output.py
@@ -0,0 +1,63 @@
+"C:\python 3.10\python.exe" C:/Users/Legion/PycharmProjects/Assn4.21107027/Assn4.21107027.py
+Solution 1
+Enter your marks: 34
+Grade : E
+Enter your marks: 56
+Grade : C
+Enter your marks: 87
+Grade : A
+Enter your marks: 50
+Grade : C
+Enter your marks: 80
+Grade : A
+Enter your marks: 66
+Grade : B
+Your choice for choosing marks and getting corresponding grade is over 
+
+
+Solution 2
+Enter year number: 345
+it is not a leap year
+
+
+Solution 3
+Welcome to multiplication game program
+Question number 1 ,  20 X 4 is
+Type your answer: 80
+Right Answer!
+Question number 2 ,  10 X 4 is
+Type your answer: 40
+Right Answer!
+Question number 3 ,  11 X 3 is
+Type your answer: 33
+Right Answer!
+Question number 4 ,  1 X 1 is
+Type your answer: 1
+Right Answer!
+Question number 5 ,  19 X 2 is
+Type your answer: 38
+Right Answer!
+Question number 6 ,  10 X 0 is
+Type your answer: 0
+Right Answer!
+Question number 7 ,  13 X 8 is
+Type your answer: 104
+Right Answer!
+Question number 8 ,  6 X 19 is
+Type your answer: 114
+Right Answer!
+Question number 9 ,  18 X 5 is
+Type your answer: 90
+Right Answer!
+Question number 10 ,  12 X 14 is
+Type your answer: 168
+Right Answer!
+Number of questions are over, great learning!
+
+
+Solution 4
+There are 177 Candies in total
+
+
+
+Process finished with exit code 0
