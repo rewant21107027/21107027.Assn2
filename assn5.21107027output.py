49effcf7dd697b368b43ddf20b4cc5404912c978
rewant21107027 <105057094+rewant21107027@users.noreply.github.com>
2022-06-15 18:19:05 +0530

    Create assn5.21107027output.py

---
+++ assn5.21107027output.py
@@ -0,0 +1,190 @@
+"C:\python 3.10\python.exe" C:/Users/Legion/PycharmProjects/ASSN5.py/Assn.py
+enter the string you want to reverse: i am learning python right now
+My input string is:  i am learning python right now
+My reverse of input string is :  won thgir nohtyp gninrael ma i
+
+
+enter initial entry of range: 1
+enter one number more you want to see according to last number in range: 10
+enter number that you would divide on items of range: 2
+2
+4
+6
+8
+
+
+enter 1st side you want for triangle: 1
+enter 2nd side you want for triangle: 1
+enter 3rd side you want for triangle: 1
+The area of triangle is:  0.4330127018922193
+enter 1st side you want for triangle: 4
+enter 2nd side you want for triangle: 5
+enter 3rd side you want for triangle: 6
+The area of triangle is:  9.921567416492215
+enter 1st side you want for triangle: 4
+enter 2nd side you want for triangle: 4
+enter 3rd side you want for triangle: 9
+Triangle is not possible according to entered inputs
+enter 1st side you want for triangle: 2
+enter 2nd side you want for triangle: 5
+enter 3rd side you want for triangle: 7
+Triangle is not possible according to entered inputs
+enter 1st side you want for triangle: 3
+enter 2nd side you want for triangle: 3
+enter 3rd side you want for triangle: 3
+The area of triangle is:  3.897114317029974
+You have only 5 opportunities to calculate area of triangle
+
+
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+
+enter the number of rows you want to see: 34
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYABC
+DEFGHIJK
+LMNOPQRST
+UVWXYABCDE
+FGHIJKLMNOP
+QRSTUVWXYABC
+DEFGHIJKLMNOP
+QRSTUVWXYABCDE
+FGHIJKLMNOPQRST
+UVWXYABCDEFGHIJK
+LMNOPQRSTUVWXYABC
+DEFGHIJKLMNOPQRSTU
+VWXYABCDEFGHIJKLMNO
+PQRSTUVWXYABCDEFGHIJ
+KLMNOPQRSTUVWXYABCDEF
+GHIJKLMNOPQRSTUVWXYABC
+DEFGHIJKLMNOPQRSTUVWXYA
+BCDEFGHIJKLMNOPQRSTUVWXY
+ABCDEFGHIJKLMNOPQRSTUVWXY
+ABCDEFGHIJKLMNOPQRSTUVWXYA
+BCDEFGHIJKLMNOPQRSTUVWXYABC
+DEFGHIJKLMNOPQRSTUVWXYABCDEF
+GHIJKLMNOPQRSTUVWXYABCDEFGHIJ
+KLMNOPQRSTUVWXYABCDEFGHIJKLMNO
+PQRSTUVWXYABCDEFGHIJKLMNOPQRSTU
+VWXYABCDEFGHIJKLMNOPQRSTUVWXYABC
+DEFGHIJKLMNOPQRSTUVWXYABCDEFGHIJK
+LMNOPQRSTUVWXYABCDEFGHIJKLMNOPQRST
+
+
+choose your lower and upper input range members
+enter the lower input of range: 2
+enter the upper input of range: 12
+My range is:  (2, 12)
+2
+3
+5
+7
+11
+
+
+following numbers are divisible by 11 and multiple of 7
+77
+154
+231
+308
+385
+462
+
+
+you can enter 10 numbers
+enter1 integer: 1
+enter2 integer: 2
+enter3 integer: 3
+enter4 integer: 4
+enter5 integer: 5
+enter6 integer: -4
+enter7 integer: -6
+enter8 integer: 2
+enter9 integer: 3
+enter10 integer: 4
+my list is  [1, 2, 3, 4, 5, -4, -6, 2, 3, 4]
+soln of part a, positive numbers 
+1
+2
+3
+4
+5
+2
+3
+4
+none
+soln of part b, negative numbers
+-4
+-6
+none
+soln of part c, odd numbers
+1
+none
+3
+none
+5
+none
+none
+none
+3
+none
+soln of part d, even numbers
+none
+2
+none
+4
+none
+-4
+-6
+2
+none
+4
+soln of part e, number of times element occurs in list
+1  occurred  1  times
+2  occurred  2  times
+3  occurred  2  times
+4  occurred  2  times
+5  occurred  1  times
+-4  occurred  1  times
+-6  occurred  1  times
+2  occurred  2  times
+3  occurred  2  times
+4  occurred  2  times
+you can enter only 10 words in the list
+enter1 word: python
+enter2 word: c
+enter3 word: c
+enter4 word: pyhton 
+enter5 word: c++
+enter6 word: java
+enter7 word: c
+enter8 word: c++
+enter9 word: python 
+enter10 word: javascript
+list entered by user is:  ['python', 'c', 'c', 'pyhton ', 'c++', 'java', 'c', 'c++', 'python ', 'javascript']
+python occurred  1  times
+c occurred  3  times
+c occurred  3  times
+pyhton  occurred  1  times
+c++ occurred  2  times
+java occurred  1  times
+c occurred  3  times
+c++ occurred  2  times
+python  occurred  1  times
+javascript occurred  1  times
+
+Process finished with exit code 0
