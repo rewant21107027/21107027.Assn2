fe67761c0a2aac688ed90d39af0fad2e100cff8d
rewant21107027 <105057094+rewant21107027@users.noreply.github.com>
2022-07-08 22:41:59 +0530

    Create assn6.21107027 output.py

---
+++ assn6.21107027 output.py
@@ -0,0 +1,66 @@
+"C:\python 3.10\python.exe" C:/Users/Legion/PycharmProjects/Assn6..py/main.py
+a perfect number is a number that is half the sum of all of its positive divisors (including itself).
+enter the number you want to check is perfect or not: 21
+list of proper divisors is:  [1, 3, 7]
+number taken is not perfect
+
+
+This is a program to check whether a passed string is palindrome or not. 
+Enter the string you want to check is a palindrome or not: my name is rewant
+entered string is not a palindrome
+
+
+enter the number of rows: 12
+             1.0 
+            1.0 1.0 
+           1.0 2.0 1.0 
+          1.0 3.0 3.0 1.0 
+         1.0 4.0 6.0 4.0 1.0 
+        1.0 5.0 10.0 10.0 5.0 1.0 
+       1.0 6.0 15.0 20.0 15.0 6.0 1.0 
+      1.0 7.0 21.0 35.0 35.0 21.0 7.0 1.0 
+     1.0 8.0 28.0 56.0 70.0 56.0 28.0 8.0 1.0 
+    1.0 9.0 36.0 84.0 126.0 126.0 84.0 36.0 9.0 1.0 
+   1.0 10.0 45.0 120.0 210.0 252.0 210.0 120.0 45.0 10.0 1.0 
+  1.0 11.0 55.0 165.0 330.0 462.0 462.0 330.0 165.0 55.0 11.0 1.0 
+
+
+This is a python function to check whether a string is pangram or not 
+Pangram are words or sentences containing every letter of the alphabet at least once
+Enter the input string you want to check for pangram: racecar madam refer
+NO, this is not a pangram
+
+
+Enter hyphen-separated sequence of words : my-name-is-rewant
+is-my-name-rewant
+
+
+
+Student ID: 21107027
+Student Name: Rewant Pahuja
+
+Student ID: 21107027
+Student Name: Rewant Pahuja
+
+Student Name: Rewant Pahuja
+Student Class: 2nd Semester
+
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+
+[[-10, 2, 8]]
+[[-10, 2, 8], [-7, -3, 10]]
+
+
+enter : []{}
+[]{} - is balanced
+
+Process finished with exit code 0
